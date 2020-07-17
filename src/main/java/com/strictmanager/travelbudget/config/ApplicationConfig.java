6054ac30abfb8f0fa1fcb862455a867e7af99372
godong9 <godong9@gmail.com>
2020-07-17 17:46:41 +0900

    Add Swagger and DB settings

---
+++ src/main/java/com/strictmanager/travelbudget/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.strictmanager.travelbudget.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@EnableJpaAuditing
+@Configuration
+public class ApplicationConfig {
+}
