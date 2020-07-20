ace75977db75614bf99ca64a98734f3e17d61e23
KIYEON KIM <giyeon15@gmail.com>
2020-07-20 16:02:33 +0900

    Mysql create issue solve
    issue:
    ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'VISIBLE

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -26,10 +26,10 @@ CREATE TABLE IF NOT EXISTS `travel_budget_db`.`trip_plan`
     `create_dt`      DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_user_id` BIGINT          NOT NULL,
     `update_dt`      DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `is_delete`      ENUM ("Y", "N") NOT NULL,
+    `is_delete`      ENUM ('Y', 'N') NOT NULL,
     `budget_id`      BIGINT          NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_trip_plan_budget1_idx` (`budget_id` ASC) VISIBLE
+    INDEX `fk_trip_plan_budget1_idx` (`budget_id` ASC)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
@@ -67,9 +67,9 @@ CREATE TABLE IF NOT EXISTS `travel_budget_db`.`trip_member`
     `trip_plan_id` BIGINT   NOT NULL,
     `budget_id`    BIGINT   NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_trip_member_trip_plan1_idx` (`trip_plan_id` ASC) VISIBLE,
-    INDEX `fk_trip_member_user1_idx` (`user_id` ASC) VISIBLE,
-    INDEX `fk_trip_member_budget1_idx` (`budget_id` ASC) VISIBLE
+    INDEX `fk_trip_member_trip_plan1_idx` (`trip_plan_id` ASC),
+    INDEX `fk_trip_member_user1_idx` (`user_id` ASC),
+    INDEX `fk_trip_member_budget1_idx` (`budget_id` ASC)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `travel_budget_db`.`trip_member`
 CREATE TABLE IF NOT EXISTS `travel_budget_db`.`category`
 (
     `id`        BIGINT                                    NOT NULL AUTO_INCREMENT,
-    `name`      ENUM ("C1", "C2", "C3", "C4", "C5", "C6") NOT NULL,
+    `name`      ENUM ('C1', 'C2', 'C3', 'C4', 'C5', 'C6') NOT NULL,
     `icon_img`  VARCHAR(1000)                             NULL,
     `create_dt` DATETIME                                  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_dt` DATETIME                                  NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -109,8 +109,8 @@ CREATE TABLE IF NOT EXISTS `travel_budget_db`.`payment_case`
     `category_id`    BIGINT       NOT NULL,
     `budget_id`      BIGINT       NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_pay_case_category1_idx` (`category_id` ASC) VISIBLE,
-    INDEX `fk_payment_case_budget1_idx` (`budget_id` ASC) VISIBLE
+    INDEX `fk_pay_case_category1_idx` (`category_id` ASC),
+    INDEX `fk_payment_case_budget1_idx` (`budget_id` ASC)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
