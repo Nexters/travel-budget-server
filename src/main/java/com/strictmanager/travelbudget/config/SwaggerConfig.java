eb0a9b4b3113fe7601563b1a0fd6113dc99c1068
KIYEON KIM <giyeon15@gmail.com>
2020-07-24 01:28:21 +0900

    Add JWT authorization setting in swaggerConfig.java

--- src/main/java/com/strictmanager/travelbudget/config/SwaggerConfig.java
+++ src/main/java/com/strictmanager/travelbudget/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.strictmanager.travelbudget.config;
 
+import com.google.common.collect.Lists;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,16 +20,39 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private final static String AUTHORIZATION = "Authorization";
+
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
             .groupName("api")
             .apiInfo(this.apiInfo())
+            .securitySchemes(Lists.newArrayList(apiKey()))
+            .securityContexts(Lists.newArrayList(securityContext()))
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.strictmanager.travelbudget.web"))
             .paths(PathSelectors.ant("/api/**"))
             .build();
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.any())
+            .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Travel Budget API")
