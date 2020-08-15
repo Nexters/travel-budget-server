0e5dce7f9e8358fcd48d2a942c5d39b649ce2f15
giyeon95 <giyeon15@gmail.com>
2020-08-15 13:13:53 +0900

    fixes #41

--- src/main/java/com/strictmanager/travelbudget/domain/member/MemberException.java
+++ src/main/java/com/strictmanager/travelbudget/domain/member/MemberException.java
@@ -16,6 +16,7 @@ public class MemberException extends RuntimeException {
     public enum MemberMessage {
         INVITE_CODE_INVALID("초대코드가 유효하지 않은걸요 :)"),
         IS_JOINED_MEMBER("이미 방에 존재하는 사용자에요 :)"),
+        CAN_NOT_JOIN_PERSONAL_PLAN("개인 여행은 사용자를 초대할 수 없어요 :)"),
         CAN_NOT_FIND_MEMBER("사용자를 찾을 수 없어요"),
         NOT_HAVE_PERMISSION("방장만 진행할 수 있어요");
 
