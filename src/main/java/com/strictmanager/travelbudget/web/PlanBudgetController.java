0efbc6195be710074debe471c1de7a708f329857
giyeon95 <giyeon15@gmail.com>
2020-08-15 17:32:28 +0900

    lazy init issue solve

--- src/main/java/com/strictmanager/travelbudget/web/PlanBudgetController.java
+++ src/main/java/com/strictmanager/travelbudget/web/PlanBudgetController.java
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,6 +52,7 @@ public class PlanBudgetController {
 
     @GetMapping("/budgets/{id}/statics")
     @ApiOperation(value = "지출 통계 조회")
+    @Transactional(readOnly = true)
     public ResponseEntity<BudgetStatisticsResponse> getBudgetStatics(
         @PathVariable(name = "id") Long budgetId
     ) {
