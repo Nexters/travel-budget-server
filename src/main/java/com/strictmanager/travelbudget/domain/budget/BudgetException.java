2417026c95bb9da1d7bac7fe7c15fbf64c28f001
giyeon95 <giyeon15@gmail.com>
2020-08-15 15:47:04 +0900

    Exception Message Confirm

--- src/main/java/com/strictmanager/travelbudget/domain/budget/BudgetException.java
+++ src/main/java/com/strictmanager/travelbudget/domain/budget/BudgetException.java
@@ -11,7 +11,7 @@ public class BudgetException extends RuntimeException {
 
     @Getter
     public enum BudgetMessage {
-        EDIT_ONLY_MINE("본인의 예산만 수정이 가능해요"),
+        EDIT_ONLY_MINE("본인의 예산만 수정할 수 있어요"),
         CAN_NOT_FIND_BUDGET("예산 정보를 찾을 수 없어요");
 
         private final String msg;
