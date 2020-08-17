5f6b3ef2f0d863dba2fa94809122e8b8336ff95c
giyeon95 <giyeon15@gmail.com>
2020-08-17 17:22:48 +0900

    GET /plans replace with 'jpql' in 'JPA query method'

--- src/main/java/com/strictmanager/travelbudget/domain/plan/PlanService.java
+++ src/main/java/com/strictmanager/travelbudget/domain/plan/PlanService.java
@@ -5,11 +5,12 @@ import com.strictmanager.travelbudget.domain.plan.PlanException.PlanMessage;
 import com.strictmanager.travelbudget.domain.user.User;
 import com.strictmanager.travelbudget.infra.persistence.jpa.TripMemberRepository;
 import com.strictmanager.travelbudget.infra.persistence.jpa.TripPlanRepository;
-import java.time.LocalDate;
+import com.strictmanager.travelbudget.infra.persistence.predicate.TripMemberPredicate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,27 +21,29 @@ public class PlanService {
     private final TripPlanRepository tripPlanRepository;
     private final TripMemberRepository tripMemberRepository;
 
-     public List<TripPlan> getComingPlans(User user) {
-        return tripMemberRepository.findByUserAndTripPlanEndDateAfter(user, LocalDate.now())
+    public List<TripPlan> getComingPlans(User user) {
+        return StreamSupport.stream(
+            tripMemberRepository.findAll(TripMemberPredicate
+                .searchComingPlans(user)).spliterator(), false)
             .map(TripMember::getTripPlan)
-            .filter(tripPlan -> tripPlan.getStartDate().compareTo(LocalDate.now()) > 0)
-            .filter(tripPlan -> tripPlan.getIsDelete().equals(YnFlag.N))
             .sorted(Comparator.comparing(TripPlan::getStartDate))
             .collect(Collectors.toList());
     }
 
     public List<TripPlan> getDoingPlans(User user) {
-        return tripMemberRepository.findByUserAndTripPlanStartDateLessThanEqualAndTripPlanEndDateGreaterThanEqual(user, LocalDate.now(), LocalDate.now())
+        return StreamSupport.stream(
+            tripMemberRepository.findAll(TripMemberPredicate
+                .searchDoingPlans(user)).spliterator(), false)
             .map(TripMember::getTripPlan)
-            .filter(tripPlan -> tripPlan.getIsDelete().equals(YnFlag.N))
             .sorted(Comparator.comparing(TripPlan::getStartDate))
             .collect(Collectors.toList());
     }
 
     public Stream<TripPlan> getFinishPlans(User user) {
-        return tripMemberRepository.findByUserAndTripPlanEndDateBefore(user, LocalDate.now())
+        return StreamSupport.stream(
+            tripMemberRepository.findAll(TripMemberPredicate
+                .searchFinishedPlans(user)).spliterator(), false)
             .map(TripMember::getTripPlan)
-            .filter(tripPlan -> tripPlan.getIsDelete().equals(YnFlag.N))
             .sorted(Comparator.comparing(TripPlan::getStartDate).reversed());
     }
 
@@ -57,7 +60,6 @@ public class PlanService {
     public TripPlan savePlan(TripPlan tripPlan) {
         return tripPlanRepository.save(tripPlan);
     }
-
 
 
 }
--- src/main/java/com/strictmanager/travelbudget/infra/persistence/jpa/TripMemberRepository.java
+++ src/main/java/com/strictmanager/travelbudget/infra/persistence/jpa/TripMemberRepository.java
@@ -3,25 +3,14 @@ package com.strictmanager.travelbudget.infra.persistence.jpa;
 import com.strictmanager.travelbudget.domain.plan.TripMember;
 import com.strictmanager.travelbudget.domain.plan.TripPlan;
 import com.strictmanager.travelbudget.domain.user.User;
-import java.time.LocalDate;
 import java.util.Optional;
-import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
-
-    //여행 예정 목록
-    Stream<TripMember> findByUserAndTripPlanEndDateAfter(User user, LocalDate nowDate);
-
-    //여행중인 목록
-    Stream<TripMember> findByUserAndTripPlanStartDateLessThanEqualAndTripPlanEndDateGreaterThanEqual(
-        User user, LocalDate nowDate, LocalDate nowDate2);
-
-    //다녀온 여행 목록
-    Stream<TripMember> findByUserAndTripPlanEndDateBefore(User user, LocalDate nowDate);
-
+public interface TripMemberRepository extends JpaRepository<TripMember, Long>,
+    QuerydslPredicateExecutor<TripMember> {
 
     Optional<TripMember> findByUserAndTripPlan(User user, TripPlan tripPlan);
 }
